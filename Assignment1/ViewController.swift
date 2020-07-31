6a4f763a6481f331c74a3b37a83f098b5d1eb141
Peter2707 <s3848409@student.rmit.edu.au>
2020-07-31 19:11:18 +1000

    Initial Commit

---
+++ Assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1
+//
+//  Created by Phearith on 31/7/20.
+//  Copyright © 2020 RMIT-iOS-s3848409-s3813756. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
