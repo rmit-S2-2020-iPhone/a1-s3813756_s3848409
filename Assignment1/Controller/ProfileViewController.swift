a256cbbb53c6acccd087b5763e3ba94ef77b4636
Peter2707 <s3848409@student.rmit.edu.au>
2020-08-19 17:13:02 +1000

    connected some buttons, labels to controllers

---
+++ Assignment1/Controller/ProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  Assignment1
+//
+//  Created by Phearith on 19/8/20.
+//  Copyright © 2020 RMIT-iOS-s3848409-s3813756. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var editProfileButton: UIButton!
+    @IBOutlet weak var moreButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Assignment1/Controller/ViewController.swift
+++ Assignment1/Controller/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     //Mark: Properties
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
