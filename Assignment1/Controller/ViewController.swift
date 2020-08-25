d80df0262477ab66905bb3a18ccf77033fca5ebe
Peter2707 <s3848409@student.rmit.edu.au>
2020-08-25 18:46:08 +1000

    added basic database

--- Assignment1/Controller/ViewController.swift
+++ Assignment1/Controller/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 import Charts
 import CoreData
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITableViewDataSource {
 
+    
+    
+    //project variables
+    var items = [Item]()
+    var object:NSManagedObjectContext!
+    var appDelegate = UIApplication.shared.delegate as? AppDelegate
+    var selectedType = ""
+    
+    
     
     
     //profile scene
@@ -37,12 +46,20 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var todayExpense: UILabel!
     @IBOutlet weak var homeMoreButton: UIButton!
     @IBOutlet weak var homeSegmentControl: UISegmentedControl!
+    @IBOutlet weak var homeTableView: UITableView!
     
     
     //add scene
-    let expenseType = ["Foods","Services","Utilities","Rent"]
+    let expenseType = ["Foods","Services","Utilities","Rent","Groceries"]
     
+    @IBOutlet weak var itemPrice: UITextField!
     @IBOutlet weak var expenseTypePicker: UIPickerView!
+    @IBAction func addFoodToDatabase(_ sender: UIButton) {
+        saveItem()
+    }
+    @IBOutlet weak var itemNote: UITextField!
+    @IBOutlet weak var itemDate: UIDatePicker!
+    
     
     
     //setting scene
@@ -52,6 +69,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        object = appDelegate?.persistentContainer.viewContext
+        self.homeTableView?.dataSource = self
+        loadData()
         
         //profile scene
         userImage?.layer.borderWidth = 1
@@ -71,7 +92,116 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
 
     
-    // add scene
+    
+    
+    
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadData()
+    }
+    
+    func loadData() {
+        // 1
+        let itemRequest:NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // 2
+        let sortDescriptor = NSSortDescriptor(key: "date", ascending: false)
+        itemRequest.sortDescriptors = [sortDescriptor]
+        
+        // 3
+        do {
+            try items = object.fetch(itemRequest)
+            
+        }catch {
+            print("Could not load data")
+        }
+        
+        // 4
+        self.homeTableView?.reloadData()
+    }
+    
+    func saveItem() {
+        let item = Item(context: object)
+        let num :Double = (itemPrice.text! as NSString).doubleValue
+        let priceField = Double(num)
+        
+        
+        item.price = priceField
+        item.name = itemNote.text
+        item.date = NSDate() as Date
+        
+        if (selectedType == "Foods") {
+            item.type = "Foods"
+        }else if (selectedType == "Services") {
+            item.type = "Services"
+        }else if (selectedType == "Utilities") {
+            item.type = "Utilities"
+        }else if (selectedType == "Rent") {
+            item.type = "Rent"
+        }else if (selectedType == "Groceries") {
+            item.type = "Groceries"
+        }else{
+            item.type = "Others"
+        }
+        appDelegate?.saveContext()
+        
+        loadData()
+        
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ homeTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ homeTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = homeTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // 1
+        let tableItem = items[indexPath.row]
+        
+        // 2
+        let itemType = tableItem.type
+        cell.textLabel?.text = itemType
+        
+        // 3
+        let itemDate = tableItem.date as! Date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d yyyy, hh:mm"
+        
+        cell.detailTextLabel?.text = dateFormatter.string(from: itemDate)
+        
+        
+        
+        return cell
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //////////////////add scene//////////////////
+    
     func numberOfComponents(in expenseTypePicker: UIPickerView) -> Int {
         return 1
     }
@@ -84,9 +214,18 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return expenseType[row]
     }
     
+    func pickerView(_ expenseTypePicker: UIPickerView, didSelectRow row: Int, inComponent  component: Int) {
+        selectedType = String(expenseType[row])
+    }
     
     
-    //stat scene
+    
+    
+    
+    
+    //////////////////stat scene//////////////////
+    
+    
     func customizeChart(dataPoints: [String], values: [Double]) {
         
         // 1. Set ChartDataEntry
@@ -122,5 +261,22 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
         return colors
     }
+    
+    
+    
+    
+    //////////////////profile scene//////////////////
+    
+    
+    
+    
+    
+    //////////////////home scene//////////////////
+    
+    
+    
+    
+    //////////////////setting scene//////////////////
+    
 }
 
