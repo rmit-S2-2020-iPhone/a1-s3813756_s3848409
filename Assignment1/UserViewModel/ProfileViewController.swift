5b13d81babc6772665ae9f4bd29a885312f2ac98
Peter2707 <s3848409@student.rmit.edu.au>
2020-10-07 21:04:24 +1100

    fixed bug where user pic kept changing back after reset name

--- Assignment1/UserViewModel/ProfileViewController.swift
+++ Assignment1/UserViewModel/ProfileViewController.swift
@@ -20,6 +20,7 @@ class ProfileViewController: UIViewController , UINavigationControllerDelegate, 
     }
     
     var picker = UIImagePickerController();
+    var imagePicker = UIImagePickerController()
     var alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
     var viewController: UIViewController?                       //declare necessary controller for choosing image process
     var pickImageCallback : ((UIImage) -> ())?;
@@ -33,20 +34,112 @@ class ProfileViewController: UIViewController , UINavigationControllerDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setProfilePic()
+        setProfileLayout()
         profileSum()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        setProfilePic()
+        setProfileLayout()
         profileSum()
-        //call neccessary function that needs to be updated
+    }
+    
+    func profileSum() {
+        let (userCurrentName, monthAmount, remainBudget, userBudget, userPic) = userViewModel.getProfileSum()
+        remainingBudget?.text = remainBudget                                                    //set those values to label
+        thisMonthExpense?.text = monthAmount
+        monthBudget?.text = userBudget
+        userName?.text = userCurrentName
+        userImage?.image = userPic
+    }
+    
+    func setProfileLayout() {
+        userImage?.layer.borderWidth = 2
+        userImage?.backgroundColor = UIColor.white
+        userImage?.layer.masksToBounds = false                                      //set user image layout
+        userImage?.layer.borderColor = UIColor.black.cgColor
+        userImage?.layer.cornerRadius = (userImage?.frame.height)!/2
+        userImage?.clipsToBounds = true
+        
+        profileSumView?.forEach {(view) in
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()                              //set style for profile page
+                view.layer.cornerRadius = 10
+            })
+        }
+    }
+    
+    func editProfile() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Edit User Name", style: .default) { _ in
+            self.editName()
+        })
+        
+        alert.addAction(UIAlertAction(title: "Change User Image", style: .default) { _ in
+            self.editUserPic()
+        })
+        
+        alert.addAction(UIAlertAction(title: "Change Budget", style: .default) { _ in
+            self.changeBudget()
+        })
+        
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction)in
+            print("User clicked Dismiss button")
+        }))
+        
+        present(alert, animated: true)
     }
     
     
-    //set user profile picture function
-    var imagePicker = UIImagePickerController()
+    func editName() {
+        let alert = UIAlertController(title: "What's your name?", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))                   //call alert function when editname function is triggered
+        
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Input your name here..."                                           //set a textfield for user input
+        })
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            let strCheck:String? = alert.textFields?.first?.text
+            if (strCheck?.trim() == "") {
+                self.popUpAlert(withTitle: "Error", message: "Name cannot be empty.")
+            }else{
+                self.userViewModel.changeUserName(alert.textFields?.first?.text ?? "User")
+                self.profileSum()
+            }
+        }))
+        
+        self.present(alert, animated: true)
+    }
+    
+    func changeBudget() {
+        let alert = UIAlertController(title: "Please enter your budget below", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))                                   //alert message
+        
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Input your budget here..."                                                         //set a textfield for the alert
+            textField.keyboardType = .decimalPad                                                                        //set keyboard type for textfield
+        })
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in            //add done action after user finished adding budget
+            let strCheck:String = alert.textFields!.first!.text!
+            if (strCheck.trim() == "" || strCheck.trim() == "."){
+                self.popUpAlert(withTitle: "Error", message: "Please enter a value.")                           //check for user's incorrect input
+            }else {
+                let dotCount = strCheck.filter({ $0 == "." }).count
+                if ( dotCount > 1) {
+                    self.popUpAlert(withTitle: "Error", message: "Value is invalid.")
+                }else {
+                    self.userViewModel.changeBudget(strCheck)
+                    self.profileSum()
+                }
+            }                                                                                                   //set new user budget to label
+        }))
+        
+        self.present(alert, animated: true)
+    }
+    
     
     func editUserPic() {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -81,7 +174,12 @@ class ProfileViewController: UIViewController , UINavigationControllerDelegate, 
     
     func openPhoneCamera() {
         let cameraViewController = CameraViewController { [weak self] image, asset in
-            self!.userImage?.image = image
+            if image == nil {
+                self?.dismiss(animated: true, completion: nil)
+            }else {
+                self!.userViewModel.changeUserImage(image!)
+            }
+            self!.profileSum()
             self?.dismiss(animated: true, completion: nil)
         }
         present(cameraViewController, animated: true, completion: nil)
@@ -97,108 +195,9 @@ class ProfileViewController: UIViewController , UINavigationControllerDelegate, 
     }
     
     
-    //set up user profile layout
-    func setProfilePic() {
-        userImage?.layer.borderWidth = 2
-        userImage?.backgroundColor = UIColor.white
-        userImage?.layer.masksToBounds = false                                      //set user image layout
-        userImage?.layer.borderColor = UIColor.black.cgColor
-        userImage?.layer.cornerRadius = (userImage?.frame.height)!/2
-        userImage?.clipsToBounds = true
-        
-        profileSumView?.forEach {(view) in
-            UIView.animate(withDuration: 0.3, animations: {
-                self.view.layoutIfNeeded()                              //set style for profile page
-                view.layer.cornerRadius = 10
-            })
-        }
-    }
-    
     func deleteCurrentImage() {
         userViewModel.deleteCurrentImage()
         profileSum()
-    }
-    
-    func editProfile() {
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        
-        alert.addAction(UIAlertAction(title: "Edit User Name", style: .default) { _ in
-            self.editName()
-        })
-        
-        alert.addAction(UIAlertAction(title: "Change User Image", style: .default) { _ in
-            self.editUserPic()
-        })
-        
-        alert.addAction(UIAlertAction(title: "Change Budget", style: .default) { _ in
-            self.changeBudget()
-        })
-        
-        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction)in
-            print("User clicked Dismiss button")
-        }))
-        
-        present(alert, animated: true)
-    }
-    
-    
-    //edit user name
-    func editName() {
-        let alert = UIAlertController(title: "What's your name?", message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))                   //call alert function when editname function is triggered
-        
-        alert.addTextField(configurationHandler: { textField in
-            textField.placeholder = "Input your name here..."                                           //set a textfield for user input
-        })
-        
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-            self.userViewModel.changeUserName(alert.textFields?.first?.text ?? "User")
-            self.profileSum()
-                                                     //set a button after finish editing
-        }))
-        
-        self.present(alert, animated: true)
-    }
-    
-    
-    //calculate expense for this month
-    func profileSum() {
-        let (userCurrentName, monthAmount, remainBudget, userBudget, userPic) = userViewModel.getProfileSum()
-        remainingBudget?.text = remainBudget                                                    //set those values to label
-        thisMonthExpense?.text = monthAmount
-        monthBudget?.text = userBudget
-        userName?.text = userCurrentName
-        userImage?.image = userPic
-    }
-    
-    
-    
-    //user budget function
-    func changeBudget() {
-        let alert = UIAlertController(title: "Please enter your budget below", message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))                                   //alert message
-        
-        alert.addTextField(configurationHandler: { textField in
-            textField.placeholder = "Input your budget here..."                                                         //set a textfield for the alert
-            textField.keyboardType = .decimalPad                                                                        //set keyboard type for textfield
-        })
-        
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in            //add done action after user finished adding budget
-            let strCheck:String = alert.textFields!.first!.text!
-            if (strCheck.trim() == "" || strCheck.trim() == "."){
-                self.popUpAlert(withTitle: "Error", message: "Please enter a value.")                           //check for user's incorrect input
-            }else {
-                let dotCount = strCheck.filter({ $0 == "." }).count
-                if ( dotCount > 1) {
-                    self.popUpAlert(withTitle: "Error", message: "Value is invalid.")
-                }else {
-                    self.userViewModel.changeBudget(strCheck)
-                    self.profileSum()
-                }
-            }                                                                                                   //set new user budget to label
-        }))
-        
-        self.present(alert, animated: true)
     }
 
 }
